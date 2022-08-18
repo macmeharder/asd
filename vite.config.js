39d145877da9417b37444e8389ef74ef9c01203a
macmeharder <kobylan@localhost>
2022-08-18 22:50:30 +0600

    q

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "KazHackStan",
+        short_name: "KazHackStan",
+        theme_color: "#e9493d",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+      },
+    }),]
 })
